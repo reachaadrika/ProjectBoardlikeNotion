922f49f206c56e4e019ae69c14e7f7aeb0822ec5
reachaadrika <aadrikabhargava12@gmail.com>
2022-10-08 10:54:33 +0530

    Completed

---
+++ src/components/Cards/CardDetails/CardDetails.jsx
@@ -0,0 +1,154 @@
+import React, { useEffect, useState } from "react";
+import {
+  Calendar,
+  List,
+  Tag,
+  Trash,
+  Type,
+  X,
+} from "react-feather";
+
+import Modal from "../../Modal/Modal";
+import Editable from "../../Editabled/Editable";
+
+import "./CardDetails.css";
+
+function CardInfo(props) {
+  const colors = [
+    "#a8193d",
+    "#4fcc25",
+    "#1ebffa",
+    "#8da377",
+    "#9975bd",
+    "#cf61a1",
+    "#240959",
+  ];
+
+  const [selectedColor, setSelectedColor] = useState();
+  const [values, setValues] = useState({
+    ...props.card,
+  });
+
+  const updateTitle = (value) => {
+    setValues({ ...values, title: value });
+  };
+
+  const updateDesc = (value) => {
+    setValues({ ...values, desc: value });
+  };
+
+  const addLabel = (label) => {
+    const index = values.labels.findIndex((item) => item.text === label.text);
+    if (index > -1) return;
+
+    setSelectedColor("");
+    setValues({
+      ...values,
+      labels: [...values.labels, label],
+    });
+  };
+
+  const removeLabel = (label) => {
+    const tempLabels = values.labels.filter((item) => item.text !== label.text);
+
+    setValues({
+      ...values,
+      labels: tempLabels,
+    });
+  };
+
+  const updateDate = (date) => {
+    if (!date) return;
+
+    setValues({
+      ...values,
+      date,
+    });
+  };
+
+  useEffect(() => {
+    if (props.updateCard) props.updateCard(props.boardId, values.id, values);
+  }, [values]);
+
+  return (
+    <Modal onClose={props.onClose}>
+      <div className="cardinfo">
+        <div className="cardinfo_box">
+          <div className="cardinfo_box_title">
+            <Type />
+            <p>Title</p>
+          </div>
+          <Editable
+            defaultValue={values.title}
+            text={values.title}
+            placeholder="Enter Title"
+            onSubmit={updateTitle}
+          />
+        </div>
+
+        <div className="cardinfo_box">
+          <div className="cardinfo_box_title">
+            <List />
+            <p>Description</p>
+          </div>
+          <Editable
+            defaultValue={values.desc}
+            text={values.desc || "Add a Description"}
+            placeholder="Enter description"
+            onSubmit={updateDesc}
+          />
+        </div>
+
+        <div className="cardinfo_box">
+          <div className="cardinfo_box_title">
+            <Calendar />
+            <p>Date</p>
+          </div>
+          <input
+            type="date"
+            defaultValue={values.date}
+            min={new Date().toISOString().substr(0, 10)}
+            onChange={(event) => updateDate(event.target.value)}
+          />
+        </div>
+
+        <div className="cardinfo_box">
+          <div className="cardinfo_box_title">
+            <Tag />
+            <p>Labels</p>
+          </div>
+          <div className="cardinfo_box_labels">
+            {values.labels?.map((item, index) => (
+              <label
+                key={index}
+                style={{ backgroundColor: item.color, color: "#fff" }}
+              >
+                {item.text}
+                <X onClick={() => removeLabel(item)} />
+              </label>
+            ))}
+          </div>
+          <ul>
+            {colors.map((item, index) => (
+              <li
+                key={index + item}
+                style={{ backgroundColor: item }}
+                className={selectedColor === item ? "li_active" : ""}
+                onClick={() => setSelectedColor(item)}
+              />
+            ))}
+          </ul>
+          <Editable
+            text="Add Label"
+            placeholder="Enter label text"
+            onSubmit={(value) =>
+              addLabel({ color: selectedColor, text: value })
+            }
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default CardInfo;
